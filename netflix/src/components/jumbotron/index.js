9554f0512c2d9a5f030cba40584a2587faee9e82
Wilson <wilson.chen143@gmail.com>
2022-02-10 05:49:55 -0700

    working on jumbotron

--- netflix/src/components/jumbotron/index.js
+++ netflix/src/components/jumbotron/index.js
@@ -1,13 +1,33 @@
 import React from 'react';
-import Inner from './styles/jumbotron';
+import { Container, Inner, Pane, Title, SubTitle, Image } from './styles/jumbotron';
 
-const Jumbotron = () => {
+const Jumbotron = ({ children, direction = 'row', ...restProps }) => {  // have to have bracket becasue those are objects that we are passing down
     return (
-        <Inner>
-            <p>Hello again!</p>
+        <Inner direction={direction}>
+            {children}
         </Inner>
     )
 
-}
+};
+
+Jumbotron.Container = function JumbotronContainer({ children, ...restProps }) {
+    return <Container {...restProps}>{children}</Container>;
+};
+
+Jumbotron.Pane = function JumbotronPane({ children, ...restProps }) {
+    return <Pane {...restProps}>{children}</Pane>;
+};
+
+Jumbotron.Title = function JumbotronTitle({ children, ...restProps }) {
+    return <Title {...restProps}>{children}</Title>;
+};
+
+Jumbotron.SubTitle = function JumbotronSubTitle({ children, ...restProps }) {
+    return <SubTitle {...restProps}>{children}</SubTitle>;
+};
+
+Jumbotron.Image = function JumbotronImage({ ...restProps }) {
+    return <Image {...restProps}></Image>;
+};
 
 export default Jumbotron;
